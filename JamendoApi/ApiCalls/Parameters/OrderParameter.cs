dd580ceed727f3e4b57df17ffeac8b14efcd229d
Arne Kiesewetter <banane1996@gmx.net>
2015-12-20 11:18:08 +0100

    Add OrderParameter things

---
+++ JamendoApi/ApiCalls/Parameters/OrderParameter.cs
@@ -0,0 +1,46 @@
+﻿using JamendoApi.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JamendoApi.ApiCalls.Parameters
+{
+    /// <summary>
+    /// Represents the order parameter which is present in nearly all calls.
+    /// </summary>
+    /// <typeparam name="TOptions">The enum that contains the possible options.</typeparam>
+    public sealed class OrderParameter<TOptions> : Parameter<OrderParameter<TOptions>, TOptions>
+    {
+        /// <summary>
+        /// Gets the sort order.
+        /// </summary>
+        public SortOrder Direction { get; set; }
+
+        public override string Name
+        {
+            get { return "order"; }
+        }
+
+        public OrderParameter()
+            : base(default(TOptions))
+        { }
+
+        public OrderParameter(TOptions order)
+            : base(order)
+        { }
+
+        protected override string getValueString()
+        {
+            return ((Enum)(object)Value).GetName() + "_" + Direction.GetName();
+        }
+    }
+
+    public enum SortOrder
+    {
+        [ApiName("desc")]
+        Descending,
+
+        [ApiName("asc")]
+        Ascending
+    }
+}
---
+++ JamendoApi/Util/ApiNameAttribute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace JamendoApi.Util
+{
+    internal static class OrderNameAttributeExtension
+    {
+        private static Dictionary<FieldInfo, string> names = new Dictionary<FieldInfo, string>();
+
+        public static string GetName(this Enum enumValue)
+        {
+            var fieldInfo = enumValue.GetType().GetRuntimeField(enumValue.ToString());
+
+            if (!names.ContainsKey(fieldInfo))
+            {
+                var name = fieldInfo.GetCustomAttribute<ApiNameAttribute>(false)?.Name ?? "";
+                names.Add(fieldInfo, name);
+            }
+
+            return names[fieldInfo];
+        }
+    }
+
+    /// <summary>
+    /// Put on the fields of enums to specify the name in the API.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+    internal sealed class ApiNameAttribute : Attribute
+    {
+        private readonly string name;
+
+        /// <summary>
+        /// Gets the enum field's API name.
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="ApiNameAttribute"/> class with the given name.
+        /// </summary>
+        /// <param name="name">The enum field's API name.</param>
+        public ApiNameAttribute(string name)
+        {
+            this.name = name;
+        }
+    }
+}
